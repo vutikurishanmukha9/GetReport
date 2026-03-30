b2ab61fb173678753a9b4385f6aa02480ba4319b
unknown <vutikurishanmukha@gmail.com>
2026-03-30 10:48:05 +0530

    fix: scroll to top on route change

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -14,6 +14,7 @@ import Examples from "./pages/Examples";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
 import Contact from "./pages/Contact";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -23,6 +24,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/pricing" element={<Pricing />} />
